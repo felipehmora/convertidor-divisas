f2b1f09bae4d88fc9b1a1a4d6315857753143789
felipehmora <felipehmoraarg@gmail.com>
2024-08-19 18:25:38 -0400

    creacion de vista para api de scrapping bcv

---
+++ index.js
@@ -0,0 +1,41 @@
+//Obteniendo informacion del api
+async function getData() {
+  const url = "http://localhost:3000/";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+    const price = `<p class='text'>${json[1].replace(/\.?0+$/, "")}</p>`;
+    document.querySelector("p").insertAdjacentHTML("beforeend", price);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+getData();
+
+console.log(new Date());
+
+//Obteniendo fecha
+
+let date = new Date();
+
+let day = date.getDate();
+
+let month = date.getMonth();
+
+let year = date.getFullYear();
+
+const completeDate = day + "/" + month + "/" + year;
+
+const innerDate = `<div class='date-container'>${completeDate}</div>`;
+
+console.log(innerDate);
+
+document.getElementById("date").innerHTML = innerDate;
+
+console.log(completeDate);
